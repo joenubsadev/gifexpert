68ddd2f4d15b03d212dbacd738f56aa2ca0efea3
joenubsadev <jorgeubaque@yahoo.com>
2023-08-09 14:45:06 -0500

    04-gif-expert-app

---
+++ babel.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    esmodules: true,
+                }
+            }
+        ],
+        ['@babel/preset-react',
+            {
+                runtime: 'automatic'
+            }
+        ],
+    ],
+};
